efa3a1d20129d9f3506d03afbe5bec5666fd8a09
LU YI <tsb1050391@gmail.com>
2017-12-30 16:46:35 +0800

    First commit

--- R/appledaily.R
+++ R/appledaily.R
@@ -1,4 +1,4 @@
-#' Search appledaily
+#' Search AppleDaily
 #'
 #' @param keyword Vector of search strings.
 #' @param date_from Begin of date of news to get with format \code{"yyyy-mm-dd"}.
@@ -8,17 +8,20 @@
 #' data.table object with columns:
 #'
 #' @examples
-#' search_appledaily(c("老年", "保險"))
+#' search_appledaily(c("老年", "保險"), date_from = Sys.Date())
 #'
+#' @import pbapply
 #' @export
 search_appledaily <- function(keyword,
                               date_from = "2003-05-02",
                               date_to = Sys.Date()) {
   # date_from = "2016-10-01"
-  # keyword = c("基隆")
+  # date_to = Sys.Date()
+  # keyword = c("老年")
 
-  message(">> Searching keywords: ", keyword, "...")
   keyword <- paste0(keyword, collapse = " AND ")
+  message(">> Searching keywords: ", keyword, "...")
+
   date_from <- date_from %>%
     ymd(tz = "ROC") %>%
     strftime(tz = "ROC", "%Y%m%d")
@@ -33,26 +36,21 @@ search_appledaily <- function(keyword,
   ## Get max page
   max_page <- maxpage_appledaily(keyword, date_int)
   if (max_page == -Inf) {
-    message("No result.")
+    message("No news result found.")
     return(invisible(NULL))
   }
   message(">> Getting ", max_page, " pages...")
 
-  search_result <- data.table(
-    title = character(0L),
-    description = character(0L),
-    news_url = character(0L)
-  )
   # for (i in 1:max_page) {
   #   cat(i, " ")
   #   res <- search_appledaily_(keyword, i)
   #   search_result <- rbindlist(list(search_result, res))
   # }
   if (.Platform$OS.type == "unix") {
-    f <- function(keyword, i) {
-      cat(i, " ")
+    f <- function(keyword, i, date_int) {
+      cat(i, " ") # to show page num in mclapply
       tryCatch({
-        search_appledaily_(keyword, i)
+        search_appledaily_(keyword, i, date_int)
       }, error = function(e) {
         print(e)
         data.table()
@@ -61,24 +59,32 @@ search_appledaily <- function(keyword,
     res_list <- parallel::mclapply(X = seq_len(max_page),
                                    FUN = f,
                                    keyword = keyword,
+                                   date_int = date_int,
                                    mc.cores = 1)
     # --- not paralleling => will get status code: 500
   } else {
     stop("Unix-like system required")
   }
+
+  search_result <- data.table(
+    title = character(0L),
+    description = character(0L),
+    news_url = character(0L)
+  )
   # print(c(list(search_result), res_list)) # for debug
   search_result <- rbindlist(c(list(search_result), res_list))
   search_result[, news_url := news_url %>%
-                  str_extract("^.*(?=/applesearch)")] %>%  # normalize url
-    .[, c("title", "description") := NULL] # drop unused columns
+                  str_extract("^.*(?=/applesearch)")] #%>%  # normalize url
+    # .[, c("title", "description") := NULL] # drop unused columns
 
-  message("\n\n>> ", search_result[, news_url] %>% length(),
-          " news to be loaded...")
+  message("\n\n>> ", search_result %>% nrow, " news to load...")
 
-  ## Prevent inconsistent number of news, e.g., caused by connection error
-  news_click <- search_result[, get_appledaily(news_url)]
-  out <- merge(news_click,
-               search_result, by = "news_url", all.x = TRUE)
+  out <- get_appledaily(search_result[, news_url], nb_core = "auto")
+
+  # Prevent inconsistent number of news, e.g., caused by connection error
+  # news_click <- search_result[res[, news_url], on = .(news_url)]
+  # out <- merge(news_click,
+  #              search_result, by = "news_url", all.x = TRUE)
 
   message("\nDone! Got ", nrow(out), " news")
   out
@@ -120,7 +126,7 @@ search_appledaily_ <- function(keyword, page = 1, date_int) {
                 ExtFilter = character(1L),
                 sorttype = "1",
                 keyword = keyword,
-                rangedate = "[20030502 TO 20160830]",
+                rangedate = date_int,
                 totalpage = "467",
                 page = page
               ))
@@ -157,66 +163,94 @@ get_appledaily_ <- function(url) {
   # url = "http://www.appledaily.com.tw/appledaily/article/finance/20160626/37284508/"
   # url = "http://www.appledaily.com.tw/realtimenews/article/international/20160830/938362/"
   # url = "http://www.appledaily.com.tw/realtimenews/article/international/20160830/938415"
-  res <- GET(url)
+  # url = "http://www.appledaily.com.tw/realtimenews/article/entertainment/20170322/1081808"
+  ua <- user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36")
+
+  res <- GET(url, ua)
   if (identical(status_code(res), 200L)) {
     res_xml <- res %>% content(as = "parsed", encoding = "UTF-8")
   } else {
     warning("http error: ", url)
-    return(data.table())
+    return(NULL)
   }
-  ## Title
-  title <- res_xml %>%
-    html_nodes("#h1") %>%
-    html_text() %>%
-    str_trim
-  subtitle <- res_xml %>%
-    html_nodes("#h2") %>%
-    html_text() %>%
-    str_trim
-  if (length(subtitle) == 0) {subtitle <- NA_character_}
-  ## Get news content
-  news_text <- res_xml %>%
-    html_nodes("#summary") %>%
-    html_text() %>%
-    str_trim
-  if (length(news_text) == 0) {
-    content_nodes <- res_xml %>%
-      html_nodes("div.articulum.trans")
-    if (content_nodes %>% xml_find_all("//script") %>% length()) {
-      content_nodes %>% xml_find_all("//script") %>% xml_remove()
-    }
-    news_text <- content_nodes %>%
-      html_text() %>%
-      str_c("\n") %>%
-      str_trim
-  }
-  ## keywords
-  keywords <- res_xml %>%
-    html_nodes('head > meta[name="keywords"]') %>%
-    html_attr("content") %>%
-    str_trim
-  ## author
-  author <- news_text %>%
-    str_match("(?<=[(（])([^／/)）]+)[／/].*報導") %>%
-    .[,2] %>%
-    str_trim
-  ## other
-  datetime_text <- res_xml %>%
-    html_node(".gggs time") %>%
-    html_text() %>%
-    str_trim
-  if (!str_detect(datetime_text, ":")) {
-    datetime_text <- str_c(datetime_text, "00:00")
-  }
-  datetime <- datetime_text %>%
-    strptime(tz = "ROC", "%Y年%m月%d日%H:%M") %>%
-    as.POSIXct()
 
-  data.table(news_source = "蘋果日報",
-             title, subtitle, datetime, news_text, author, keywords)
+  location_301 <- res %>% content(encoding = "UTF-8") %>%
+    html_nodes("script:contains('window.location.href')")
+  if (length(location_301) == 1) {
+    url_base <- res$all_headers[[1]]$headers$location %>% parse_url() %>% .$hostname
+    new_url <- paste0("http://", url_base,
+                      location_301 %>%
+                        as.character() %>%
+                        str_extract('(?<=href=\").*(?=\"</script>)'))
+    res_xml <- GET(new_url, ua) %>% content()
+  }
+
+  tryCatch({
+    ## Title
+    title <- res_xml %>%
+      html_nodes("#h1") %>%
+      html_text() %>%
+      str_trim
+    subtitle <- res_xml %>%
+      html_nodes("#h2") %>%
+      html_text() %>%
+      str_trim
+    if (length(subtitle) == 0) {subtitle <- NA_character_}
+    ## Get news content
+    news_text <- res_xml %>%
+      html_nodes("#summary") %>%
+      html_text() %>%
+      str_trim
+    if (length(news_text) == 0) {
+      content_nodes <- res_xml %>%
+        html_nodes("div.articulum.trans")
+      if (content_nodes %>% xml_find_all("//script") %>% length()) {
+        content_nodes %>% xml_find_all("//script") %>% xml_remove()
+      }
+      news_text <- content_nodes %>%
+        html_text() %>%
+        str_c("\n") %>%
+        str_trim
+    }
+    if (nchar(news_text) == 0) {
+      news_text <- res_xml %>% html_nodes("#introid, #bcontent") %>%
+        html_text() %>%
+        str_trim %>% paste(collapse = "\n")
+    }
+    ## keywords
+    keywords <- res_xml %>%
+      html_nodes('head > meta[name="keywords"]') %>%
+      html_attr("content") %>%
+      str_trim
+    ## author
+    author <- news_text %>%
+      str_match("(?<=[【(（])([^】╱／/)）]+)[╱／/].*報導") %>%
+      .[,2] %>%
+      str_trim
+    ## other
+    datetime_text <- res_xml %>%
+      html_node(".gggs time") %>%
+      html_text() %>%
+      str_trim
+
+    if (!str_detect(datetime_text, ":")) {
+      datetime_text <- str_c(datetime_text, "00:00")
+    }
+
+    datetime <- datetime_text %>%
+      strptime(tz = "ROC", "%Y年%m月%d日%H:%M") %>%
+      as.POSIXct()
+
+    out <- data.table(news_source = "蘋果日報",
+                      title, subtitle, datetime, news_text, author, keywords)
+  }, error = function(e) {
+    out <<- NULL
+  })
+
+  out
 }
 
-get_appledaily <- function(urls) {
+get_appledaily <- function(urls, nb_core = "auto") {
   # urls = c(
   #   "http://www.appledaily.com.tw/appledaily/article/finance/20160626/37284508/",
   #   "http://www.appledaily.com.tw/realtimenews/article/international/20160830/938362/"
@@ -227,20 +261,42 @@ get_appledaily <- function(urls) {
     tryCatch({
       get_appledaily_(x)
     }, error = function(e) {
-      data.table()
+      message(e)
+      NULL
     })
   }
-  if (.Platform$OS.type == "unix") {
-    out_list <- parallel::mcmapply(FUN = f, urls,
-                                   SIMPLIFY = FALSE, USE.NAMES = TRUE,
-                                   mc.cores = parallel::detectCores()-1)
-  } else {
-    stop("Only support Unix-like")
+
+  if (nb_core == "auto") {
+    nb_core <- parallel::detectCores() - 1
   }
+  if (nb_core == 1) {
+    out_list <- sapply(urls, f,
+                       simplify  = FALSE, USE.NAMES = TRUE)
+  } else {
+    if (.Platform$OS.type == "unix") {
+      cl <- parallel::makeCluster(nb_core)
+      on.exit(parallel::stopCluster(cl))
+      parallel::clusterExport(cl, list("index", "get_appledaily_"), envir = environment())
+      parallel::clusterEvalQ(cl, {
+        library(httr)
+        library(rvest)
+        library(stringr)
+        library(data.table)
+      })
+
+      out_list <- pbapply::pbsapply(cl = cl,
+                                    urls, f,
+                                    simplify  = FALSE, USE.NAMES = TRUE)
+    } else {
+      stop("Only support Unix-like")
+    }
+  }
+
   tryCatch({
     out <- rbindlist(out_list, idcol = "news_url")
   }, error = function(e) {
-    out <- out_list
+    out <<- out_list
+    warning("Something went wrong with rbindlist(out_list)")
   })
   out
 }
--- draft/edn.R
+++ draft/edn.R
@@ -1,3 +1,29 @@
+#' Economic Daily News 經濟日報
+#'
+#' \link{http://edn.udn.com/}
+#'
+#' @param dateWant \code{"yyyymmdd"}.
+#'
+#' @return
+#' data.table object with columns:
+#'
+#' @examples
+#' getAllNewsUdn("20161001")
+#'
+#' @export
+getAllNewsEC <- function(sleepBetween = 5, sleepTime = 1, printSts=TRUE){
+  PageUrl<- getCateUrlEC()
+  UrlList <- lapply(PageUrl$href[-1], getUrlListcnEC) # 去除熱點
+  UrlList <- unname(unlist(UrlList))
+  newsList <- list()
+  for( i in 1:length(UrlList)){
+    newsList[[i]] <- getNewsEC(UrlList[i])
+    if ( (i %% sleepBetween) ==0) Sys.sleep(sleepTime)
+    if ( printSts&(i%%100 ==0) ) cat('經濟日報 ', i, 'is ok', '\n')
+  }
+  ECnews <- do.call(rbind, newsList)
+}
+
 #(1)經濟日報新聞分類URL
 getCateUrlEC <- function(){
   res <- GET('http://money.udn.com/money/index', encoding='utf8')
@@ -39,7 +65,7 @@ getNewsEC <- function(URL, sleepTime = 0){
   #newsTitle <- str_replace_all(newsTitle, '(\r|\n| )+', '')
   newsCate <- cssApply(res2,"#nav > a",cssCharacter)
   newsCate <- newsCate[length(newsCate)]
-  
+
   newsText <- cssApply(res2,"#story_body_content>p",cssCharacter)
   newsText <- paste(newsText, collapse = '')
   newsText <- str_replace_all(newsText, '(\r|\n| )+', '')
@@ -50,30 +76,30 @@ getNewsEC <- function(URL, sleepTime = 0){
   newsTime <- str_replace_all(str_extract(newsDate, '[0-9]{2}:[0-9]{2}:[0-9]{2}'), ':', '')
   newsDate <- str_replace_all(str_extract(newsDate, '[0-9]{4}-[0-9]{2}-[0-9]{2}'), '-', '')
   #newsClick <- xpathSApply(res, '//div[@class="article_star clear-fix"]/div[@class="art_click clear-fix"]/span[@class="num"]', xmlValue)[1]
-  
+
   #URLnewsShareFB <- cssApply(res2,"span.pluginCountTextDisconnected",cssCharacter) #failed
   # URLnewsDiscuss <- cssApply(res2,"li.discuss.only_web > div > b",cssCharacter)
-  
+
   URLQry <- str_replace_all(str_replace_all(str_replace_all(URL, '/', '%2F'), ':' ,'%3A'), '-', '%2D')
   URLnewsShare <- paste0('http://graph.facebook.com/fql?q=SELECT%20share_count,%20like_count,%20comment_count,%20total_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%22',URLQry,'%22','&callback=_ate.cbs.rcb_httpwwwchinatimescom0')
   newsShareContent <- str_extract(rawToChar(GET(URLnewsShare)$content), '\\{.+\\}')
   newsShareContent <- jsonlite::fromJSON(newsShareContent)
   URLnewsShare <- newsShareContent$data$total_count
   URLnewsShare <- ifelse(is.null(URLnewsShare), NA, URLnewsShare)
-  
+
   URLnewsShare2 <- paste0('http://api-public.addthis.com/url/shares.json?url=',URLQry, '&callback=_ate.cbs.rcb_httpwwwchinatimescom0')
   newsShare2Content <- rawToChar(GET(URLnewsShare2)$content)
-  newsShare2Content <- str_extract(newsShare2Content, '\\{.+\\}') 
+  newsShare2Content <- str_extract(newsShare2Content, '\\{.+\\}')
   newsShare2Content <- jsonlite::fromJSON(newsShare2Content)
   URLnewsShare2 <- newsShare2Content$shares
   URLnewsShare2 <- ifelse(is.null(URLnewsShare2), NA, URLnewsShare2)
-  result <- try(data.frame(newsWebsite='經濟日報', newsCate=newsCate, newsAuthor=newsAuthor, newsDate=newsDate, newsTime=newsTime, 
+  result <- try(data.frame(newsWebsite='經濟日報', newsCate=newsCate, newsAuthor=newsAuthor, newsDate=newsDate, newsTime=newsTime,
                            newsTitle=newsTitle, newsText=newsText,
                            newsClick=NA, newsShare=URLnewsShare+URLnewsShare2,
                            href=URL, createTime=format(Sys.time(), '%Y%m%d%H%M%S'),
                            stringsAsFactors=FALSE ), silent = TRUE)
   if( inherits(result, 'try-error')){
-    result <- data.frame(newsWebsite='經濟日報', newsCate=NA, newsAuthor=NA, newsDate=NA, newsTime=NA, 
+    result <- data.frame(newsWebsite='經濟日報', newsCate=NA, newsAuthor=NA, newsDate=NA, newsTime=NA,
                          newsTitle=NA, newsText=NA,
                          newsClick=NA, newsShare=NA,
                          href=URL, createTime=format(Sys.time(), '%Y%m%d%H%M%S'),
@@ -82,16 +108,3 @@ getNewsEC <- function(URL, sleepTime = 0){
   return(result)
 }
 
-
-getAllNewsEC <- function(sleepBetween = 5, sleepTime = 1, printSts=TRUE){
-  PageUrl<- getCateUrlEC()
-  UrlList <- lapply(PageUrl$href[-1], getUrlListcnEC) # 去除熱點
-  UrlList <- unname(unlist(UrlList))
-  newsList <- list()
-  for( i in 1:length(UrlList)){
-    newsList[[i]] <- getNewsEC(UrlList[i])
-    if ( (i %% sleepBetween) ==0) Sys.sleep(sleepTime)
-    if ( printSts&(i%%100 ==0) ) cat('經濟日報 ', i, 'is ok', '\n')
-  }
-  ECnews <- do.call(rbind, newsList)
-}
